237c4c62ae8374f99cfb0fbf77d4a1f60b683207
jijb95 <jorgeisaacjimenezb@hotmail.com>
2017-01-31 21:29:25 -0600

    terminado

---
+++ clase4/JavaScript.js
@@ -0,0 +1,19 @@
+﻿
+function rectangulo(num1, num2) {
+    document.getElementById("resr").innerHTML = parseInt(num1) * parseInt(num2) + "cm²"
+}
+function triangulo(num1, num2) {
+    var n = 2;
+    document.getElementById("rest").innerHTML = (parseInt(num1) * parseInt(num2))/n + "cm²";
+}
+function rombo(num1, num2) {
+    var n = 2;
+    document.getElementById("resb").innerHTML = (parseInt(num1) * parseInt(num2)) / n + "cm²";
+}
+function paralelo(num1, num2) {
+    document.getElementById("resr").innerHTML = parseInt(num1) * parseInt(num2);
+}
+function circulo(num1) {
+    var n = 2;
+    document.getElementById("resc").innerHTML = Math.PI * Math.pow(num1, n) + "cm²";
+}
